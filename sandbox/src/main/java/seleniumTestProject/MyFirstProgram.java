ff16a80a0aa51397f81ffd9efca5265bdc78e6c5
szhul <s_zhul@mail.ru>
2023-04-09 21:24:09 +0700

    Немного переделал FirstClass

--- sandbox/src/main/java/seleniumTestProject/MyFirstProgram.java
+++ sandbox/src/main/java/seleniumTestProject/MyFirstProgram.java
@@ -1,10 +1,19 @@
 package seleniumTestProject;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-public static void main(String[] args){
+    public static void main(String[] args) {
 
-System.out.println("Hello, world!");
+        String somebody = "world!";
 
-}
+        System.out.println("Hello, " +  somebody);
+
+        int l = 5;
+        int s = l*l;
+
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
+        Point point = new Point(4,9, 16, 25);
+        point.distance();
+    }
 }
