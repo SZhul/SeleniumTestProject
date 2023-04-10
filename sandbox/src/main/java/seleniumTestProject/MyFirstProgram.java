47bd852e79bbe37430b3aec5ad5808fe8eeecd35
szhul <s_zhul@mail.ru>
2023-04-10 21:03:59 +0700

    refactor

--- sandbox/src/main/java/seleniumTestProject/MyFirstProgram.java
+++ sandbox/src/main/java/seleniumTestProject/MyFirstProgram.java
@@ -7,17 +7,18 @@ public class MyFirstProgram {
         hello("world");
 
 
+//        Point p1 = new Point(9,4);
+//        Point p2 = new Point(16, 25);
+//        pointDistance(p1, p2);
 
 
+        Square s = new Square(5);
 
-        Square s = new Square();
-        s.l = 5;
-        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
-        Rectangle r = new Rectangle();
-        r.a = 4;
-        r.b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+        Rectangle r = new Rectangle(4, 6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
 
     }
@@ -30,30 +31,17 @@ public class MyFirstProgram {
         System.out.println("Hello " + something + " !");
     }
 
-    public static double area(double l){
-        return l*l;
-    }
 
-    public static double area(double a, double b){
-        return a*b;
-    }
 
-    public static double area(Square s){
-        return s.l * s.l;
-    }
 
-    public static double area(Rectangle a){
-        return a.a * a.b;
-    }
 
-    public static double distance(Point p1, Point p2){
+
+    public static double pointDistance(Point p1, Point p2){
         double distance = Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
         System.out.println("Расстояние между двумя  точками равно " + distance);
         return distance;
     }
 
-    Point p1 = new Point(9,4);
-    Point p2 = new Point(16, 25);
-    distance(p1, p2);
+
 
 }
--- sandbox/src/main/java/seleniumTestProject/Square.java
+++ sandbox/src/main/java/seleniumTestProject/Square.java
@@ -2,4 +2,13 @@ package seleniumTestProject;
 
 public class Square {
     public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+
+    public double area(){
+        return this.l * this.l;
+    }
+
 }
