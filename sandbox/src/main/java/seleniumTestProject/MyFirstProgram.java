a35c24f3382929985a5852950c18ea9aae718567
szhul <s_zhul@mail.ru>
2023-04-10 21:16:54 +0700

    refactor

--- sandbox/src/main/java/seleniumTestProject/MyFirstProgram.java
+++ sandbox/src/main/java/seleniumTestProject/MyFirstProgram.java
@@ -5,22 +5,10 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         hello("world");
-
-
-        Point p1 = new Point(9,4);
-        Point p2 = new Point(16, 25);
-        p1.pointDistance(p1,p2);
-
-
-
         Square s = new Square(5);
-
         System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
-
         Rectangle r = new Rectangle(4, 6);
-
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
-
 
     }
 
