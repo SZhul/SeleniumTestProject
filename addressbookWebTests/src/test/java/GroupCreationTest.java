7226d726f29e662422b82b2d2c14750b230fdab4
szhul <s_zhul@mail.ru>
2023-04-15 02:09:46 +0700

    Логин вынесен в фикстуру

--- addressbookWebTests/src/test/java/GroupCreationTest.java
+++ addressbookWebTests/src/test/java/GroupCreationTest.java
@@ -3,10 +3,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.fail;
 
 public class GroupCreationTest {
   private WebDriver wd;
@@ -18,10 +15,6 @@ public class GroupCreationTest {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) wd;
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -30,6 +23,11 @@ public class GroupCreationTest {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
