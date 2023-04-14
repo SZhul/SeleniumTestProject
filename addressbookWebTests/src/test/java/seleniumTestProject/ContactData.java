ae710d860036b50d9cd7c20e1797705c2caef875
szhul <s_zhul@mail.ru>
2023-04-15 03:08:08 +0700

    ДЗ (создание нового контакта)

---
+++ addressbookWebTests/src/test/java/seleniumTestProject/ContactData.java
@@ -0,0 +1,111 @@
+package seleniumTestProject;
+
+public class ContactData {
+    private final String name;
+    private final String secondName;
+    private final String lastName;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String home;
+    private final String mobile;
+    private final String work;
+    private final String fax;
+    private final String email;
+    private final String homepage;
+    private final String bday;
+    private final String bmont;
+    private final String byear;
+    private final String notes;
+
+    public ContactData(String name, String secondName, String lastName, String nickname, String title, String company,
+                       String address, String home, String mobile, String work, String fax, String email,
+                       String homepage, String bday, String bmont, String byear, String notes) {
+        this.name = name;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.home = home;
+        this.mobile = mobile;
+        this.work = work;
+        this.fax = fax;
+        this.email = email;
+        this.homepage = homepage;
+        this.bday = bday;
+        this.bmont = bmont;
+        this.byear = byear;
+        this.notes = notes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public String getBmont() {
+        return bmont;
+    }
+
+    public String getByear() {
+        return byear;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
--- addressbookWebTests/src/test/java/seleniumTestProject/GroupCreationTest.java
+++ addressbookWebTests/src/test/java/seleniumTestProject/GroupCreationTest.java
@@ -2,6 +2,7 @@ package seleniumTestProject;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -41,7 +42,146 @@ public class GroupCreationTest {
     submitGroupCreation();
     returnToGroupPage();
     logout();
+  }
 
+  @Test
+  public void testContactCreation() throws Exception{
+    initNewContact();
+    fillContacts(new ContactData("Вася", "Петя", "Сережа", "Sergey", "13", "123", "Tomsk", "Home", "Mobile", "Work", "Fax", "email1", "homepage", "8", "April", "1992", "Notes"));
+    goToMainPage();
+  }
+
+  private void fillContacts(ContactData contactData) {
+    fillFirstName(contactData.getName());
+    fillMiddleName(contactData.getSecondName());
+    fillLastName(contactData.getLastName());
+    fillNickName(contactData.getNickname());
+    fillTitle(contactData.getTitle());
+    fillCompany(contactData.getCompany());
+    fillAddress(contactData.getAddress());
+    fillHome(contactData.getHome());
+    fillMobile(contactData.getMobile());
+    fillWork(contactData.getWork());
+    fillFax(contactData.getFax());
+    fillEmail(contactData.getEmail());
+    fillHomepage(contactData.getHomepage());
+    selectBday(contactData.getBday());
+    selectBmonth(contactData.getBmont());
+    fillByear(contactData.getByear());
+    fillNotes(contactData.getNotes());
+    submitContactCreation();
+  }
+
+  private void goToMainPage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillNotes(String notes) {
+    wd.findElement(By.name("notes")).click();
+    wd.findElement(By.name("notes")).clear();
+    wd.findElement(By.name("notes")).sendKeys(notes);
+  }
+
+  private void fillByear(String year) {
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(year);
+  }
+
+  private void selectBmonth(String mont) {
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(mont);
+  }
+
+  private void selectBday(String day) {
+    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(day);
+  }
+
+  private void fillHomepage(String homepage) {
+    wd.findElement(By.name("homepage")).click();
+    wd.findElement(By.name("homepage")).clear();
+    wd.findElement(By.name("homepage")).sendKeys(homepage);
+  }
+
+  private void fillEmail(String email) {
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(email);
+  }
+
+  private void fillFax(String fax) {
+    wd.findElement(By.name("fax")).click();
+    wd.findElement(By.name("fax")).clear();
+    wd.findElement(By.name("fax")).sendKeys(fax);
+  }
+
+  private void fillWork(String work) {
+    wd.findElement(By.name("work")).click();
+    wd.findElement(By.name("work")).clear();
+    wd.findElement(By.name("work")).sendKeys(work);
+  }
+
+  private void fillMobile(String mobile) {
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(mobile);
+  }
+
+  private void fillHome(String home) {
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(home);
+  }
+
+  private void fillAddress(String address) {
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(address);
+  }
+
+  private void fillCompany(String company) {
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(company);
+  }
+
+  private void fillTitle(String title) {
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(title);
+  }
+
+  private void fillNickName(String nickname) {
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(nickname);
+  }
+
+  private void fillLastName(String lastName) {
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(lastName);
+  }
+
+  private void fillMiddleName(String secondName) {
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(secondName);
+  }
+
+  private void fillFirstName(String name) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(name);
+  }
+
+  private void initNewContact() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   private void logout() {
