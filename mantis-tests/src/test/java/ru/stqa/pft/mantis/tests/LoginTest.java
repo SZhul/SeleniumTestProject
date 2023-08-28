c3c09d0dc0cd258f6c54ddd896e78e8542947a2b
szhul <s_zhul@mail.ru>
2023-08-28 13:35:22 +0700

    Авторизация в mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTest.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.mantis.tests;
+
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+public class LoginTest extends TestBase {
+
+    @Test
+    public void loginTest() throws IOException {
+        HttpSession session = app.newSession();
+        assertTrue(session.login("administrator", "root"));
+        assertTrue(session.isLoggedAs("administrator"));
+    }
+
+}
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,11 +5,11 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
-import static org.openqa.selenium.remote.Browser.CHROME;
+import static org.openqa.selenium.remote.Browser.*;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", CHROME.browserName()));
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", FIREFOX.browserName()));
 
 
     @BeforeSuite
